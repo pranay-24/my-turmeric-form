d20acd24f40d9a75233bc31d1e948afa6106066f
Pranay Mangal <mangal.pranay@gmail.com>
2025-02-06 00:39:50 -0500

    w

--- src/MultiSelectForm.jsx
+++ src/MultiSelectForm.jsx
@@ -332,7 +332,7 @@ const toggleSpecialty = (value) => {
   };
   
   return (
-    <div className="w-full max-w-2xl mx-auto px-4 sm:px-6 md:px-8">
+    <div className="w-full mx-auto px-4 sm:px-6 md:px-8">
     <form onSubmit={handleSubmit} className="space-y-6">
       {/* Basic Information */}
       <div className="grid grid-cols-1 gap-5 md:grid-col-2">
